569662e5d87a258b67baf95da2c9b164f4e5554f
jmps1126 <jmps1126@gmail.com>
2019-05-19 18:30:52 -0500

    se realzia commit finalizando el entregable uno

--- index.js
+++ index.js
@@ -1,16 +1,87 @@
-//El siguiente código es un ejemplo de servidor web escrito en Node.js.
+const fs = require('fs');
+const courses = [
+    {
+        id: 1,
+        nombre: 'Matamáticas',
+        duracion: '20 horas',
+        valor: '$ 4.000.000'
+    },
+    {
+        id: 2,
+        nombre: 'Fisica',
+        duracion: '40 horas',
+        valor: '$ 5.000.000'
+    },
+    {
+        id: 3,
+        nombre: 'Geometría',
+        duracion: '60 horas',
+        valor: '$ 6.000.000'
+    }
+];
+const options = {
+    id: {
+        demand: true,
+        alias: 'i'
+    },
+    nombre: {
+        demand: true,
+        alias: 'n'
+    },
+    cedula: {
+        demand: true,
+        alias: 'c'
+    }
+};
 
-const http = require('http');
+// creación del comando para inscribir al estudiante
+const argv = require('yargs').command('inscribir', 'Matricular el curso ingresado', options).argv;
 
-const hostname = '127.0.0.1';
-const port = 3000;
+//Mostrar los cursos con intervalo de dos segundos
+function showCourses() {
+    for (let i = 0; i < courses.length; i++) {
+        setTimeout(() => {
+            let infoCourses = `El curso de ${courses[i].nombre} con número de identificación ${courses[i].id} tiene una duración de ${courses[i].duracion} y su valor es de ${courses[i].valor}\n`;
+            console.log(infoCourses);
+        }, i * 2000);
+    }
+};
 
-const server = http.createServer((req, res) => {
-    res.statusCode = 200;
-    res.setHeader('Content-Type', 'text/plain');
-    res.end('Hola Mundo\n');
-});
+// Crear el archivo con la información solicitada por la historia de usuario
+function createFile(course) {
+    const text = `El estudiante ${argv.n} identificado con el número de identificación ${argv.c}\n
+Se ha registrado exitosamente al siguiente curso:\n
+Identificación: ${course.id}\n
+Nombre: ${course.nombre}\n
+Duración: ${course.duracion}\n
+Valor: ${course.valor}`;
 
-server.listen(port, hostname, () => {
-    console.log(`Server running at http://${hostname}:${port}/`);
-});
+    fs.writeFile('infoCourse.txt', text, (err) => {
+        if (err) throw err;
+        console.log('Archivo creado exitosamente');
+    });
+};
+
+//matricular estudiante y gernarar el archivo
+function registerEstudent() {
+    // const courseSelected = courses.find((course) => { course.id == argv.i });
+    const courseSelected = courses.find((course) => { return course.id == argv.i });
+
+    console.log(courseSelected);
+
+    if (courseSelected) {
+        createFile(courseSelected);
+    } else {
+        console.log("No se encontro un curso con el id ingresado");
+    }
+};
+
+
+
+//validamos si hay algún curso seleccionado y matriculamos al estudiante si no mostramos los cursos que hay actualmente
+if (argv.i && argv.n && argv.c) {
+    registerEstudent();
+} else {
+    showCourses();
+}
+
